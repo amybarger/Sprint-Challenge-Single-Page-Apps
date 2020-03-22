3c419130c7f4a0fcdbc1f9a72917ec18ee541323
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-03-22 19:31:23 -0400

    Created axios call for Rick and Morty API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characterState, setCharacterState] = useState([])
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios
+      .get(`https://rickandmortyapi.com/api/`)
+      .then(res => {
+        setCharacterState(res.data)
+      })
+      .catch(error => {
+        console.error("Error message", error);
+      })
   }, []);
 
   return (
