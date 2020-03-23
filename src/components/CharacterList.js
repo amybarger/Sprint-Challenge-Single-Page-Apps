c48f7f6bfa378b6f6c95049c7f90bc1a7d0fc049
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-03-22 21:26:35 -0400

    Set up props, fixed some css, mapped API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Axios from "axios";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -9,10 +10,11 @@ export default function CharacterList() {
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    Axios
-      .get(`https://rickandmortyapi.com/api/`)
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
       .then(res => {
-        setCharacterState(res.data)
+        setCharacterState(res.data.results)
+        console.log("working!", res);
       })
       .catch(error => {
         console.error("Error message", error);
@@ -21,7 +23,19 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    
+      {characterState.map(character => {
+        console.log(characterState);
+        return (
+        <CharacterCard key={character.id}
+        image={character.image}
+        id={character.id}
+        name={character.name}
+        species={character.species}
+        location={character.locations}
+        />
+
+      )})}
     </section>
   );
 }
